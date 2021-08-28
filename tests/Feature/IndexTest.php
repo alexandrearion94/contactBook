4341effab707f31abf9a92be125d725f741d2298
alexandrearion94 <alexandrearion.94@gmail.com>
2021-08-28 05:33:19 -0300

    IndexTest

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -2,21 +2,41 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class IndexTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_index_result(): void
+    {
+        
+        $this->mockContact();
+        $contact = Contact::first();
+
+
+        $response = $this->get('/api/contacts');
+
+        $response->assertStatus(200)
+        ->assertJsonFragment([
+            //'data' => [
+                [
+                    'id' => $contact->id,
+                    'name' => $contact->name,
+                    'fone' => $contact->fone,
+                    'created_at' => $contact->created_at,
+                    'updated_at' => $contact->updated_at,
+                    'deleted_at' => $contact->deleted_at,
+                ]
+            ]
+        );
+    }
+    private function mockContact(int $count = 1): void
+    {
+        Contact::factory()->count($count)->create();
     }
 }
